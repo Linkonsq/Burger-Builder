e14c53fe05d06be7c729a1b86ef8149522caf61e
Spekter-2 <spekter@Spekter-2s-iMac.local>
2021-01-18 13:09:15 +0600

    Change folder structure

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import classes from './Modal.module.css';
-import Auxi from '../../../hoc/Auxi';
+import Auxi from '../../../hoc/Auxi/Auxi';
 import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component {
