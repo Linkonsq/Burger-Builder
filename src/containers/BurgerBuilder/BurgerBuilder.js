a222266f57d24a4976623e144e3f2dc77343fcd7
Linkon Siddique <linkon.sq48@gmail.com>
2021-01-20 02:54:49 +0600

    Navigating to the chekcout page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -87,28 +87,29 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     //alert('You continue!');
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Linkon Siddique',
-        address: {
-          street: 'Test street',
-          zipCode: '1941',
-          country: 'Bangladesh'
-        },
-        email: 'test@gmail.com'
-      },
-      deliveryMethod: 'fastest'
-    }
-    axios.post('/orders.json', order)
-      .then(response => {
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch(error => {
-        this.setState({ loading: false, purchasing: false });
-      });
+    // this.setState({ loading: true });
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'Linkon Siddique',
+    //     address: {
+    //       street: 'Test street',
+    //       zipCode: '1941',
+    //       country: 'Bangladesh'
+    //     },
+    //     email: 'test@gmail.com'
+    //   },
+    //   deliveryMethod: 'fastest'
+    // }
+    // axios.post('/orders.json', order)
+    //   .then(response => {
+    //     this.setState({ loading: false, purchasing: false });
+    //   })
+    //   .catch(error => {
+    //     this.setState({ loading: false, purchasing: false });
+    //   });
+    this.props.history.push('/checkout');
   }
 
   render() {
