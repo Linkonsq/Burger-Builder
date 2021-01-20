8d238d35e49e73e9d3f7a0da14276f4f5da3d02b
Linkon Siddique <linkon.sq48@gmail.com>
2021-01-20 15:30:59 +0600

    Dynamically creates inputs based on js config

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,15 +4,18 @@ import classes from './Input.module.css';
 const input = (props) => {
     let inputElement = null;
 
-    switch (props.inputtype) {
+    switch (props.elementType) {
         case ('input'):
-            inputElement = <input className={classes.InputElement} {...props} />;
+            inputElement = <input className={classes.InputElement}
+                {...props.elementConfig} value={props.value} />;
             break;
         case ('textarea'):
-            inputElement = <textarea {...props} />;
+            inputElement = <textarea className={classes.InputElement}
+                {...props.elementConfig} value={props.value} />;
             break;
         default:
-            inputElement = <input {...props} />;
+            inputElement = <input className={classes.InputElement}
+                {...props.elementConfig} value={props.value} />;
     }
 
     return (
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,11 +7,57 @@ import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
   state = {
-    name: '',
-    email: '',
-    address: {
-      street: '',
-      postalCode: ''
+    orderForm: {
+      name: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Name'
+        },
+        value: ''
+      },
+      street: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Street'
+        },
+        value: ''
+      },
+      zipCode: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'ZIP Code'
+        },
+        value: ''
+      },
+      country: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Country'
+        },
+        value: ''
+      },
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'email',
+          placeholder: 'Email'
+        },
+        value: ''
+      },
+      deliveryMethod: {
+        elementType: 'select',
+        elementConfig: {
+          options: [
+            { value: 'fastest', displayValue: 'Fastest' },
+            { value: 'cheapest', displayValue: 'Cheapest' }
+          ]
+        },
+        value: ''
+      },
     },
     loading: false
   }
@@ -22,16 +68,6 @@ class ContactData extends Component {
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.price,
-      customer: {
-        name: 'Linkon Siddique',
-        address: {
-          street: 'Test street',
-          zipCode: '1941',
-          country: 'Bangladesh'
-        },
-        email: 'test@gmail.com'
-      },
-      deliveryMethod: 'fastest'
     }
     axios.post('/orders.json', order)
       .then(response => {
@@ -44,12 +80,22 @@ class ContactData extends Component {
   }
 
   render() {
+    let formsElementArray = [];
+    for (let key in this.state.orderForm) {
+      formsElementArray.push({
+        id: key,
+        config: this.state.orderForm[key]
+      });
+    }
+
     let form = (
       <form>
-        <Input inputtype="input" type="text" name="name" placeholder="Name" />
-        <Input inputtype="input" type="email" name="email" placeholder="Email" />
-        <Input inputtype="input" type="text" name="street" placeholder="Street" />
-        <Input inputtype="input" type="text" name="postal" placeholder="Postal Code" />
+        {formsElementArray.map(formElement => (
+          <Input key={formElement.key}
+            elementType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value} />
+        ))}
         <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
       </form>
     );
